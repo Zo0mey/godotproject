eb6595ae291823a33526182271c660a751fbb9bd
burkhsas <burkhsas@students.zhaw.ch>
2019-07-03 16:09:22 +0200

    add some assets

---
+++ Player.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+const SPEED = 200
+
+var movedir = Vector2(0, 0)
+onready var sprite = get_node("AnimatedSprite")
+
+func _physics_process(delta):
+	controls_loop()
+	movement_loop()
+
+func controls_loop():
+	var LEFT 	= Input.is_action_pressed("ui_left")
+	var RIGHT	= Input.is_action_pressed("ui_right")
+	var UP		= Input.is_action_pressed("ui_up")
+	var DOWN	= Input.is_action_pressed("ui_down")
+	
+	movedir.x = -int(LEFT) + int(RIGHT)
+	movedir.y = -int(UP) + int(DOWN)
+
+func movement_loop():
+	var motion = movedir.normalized() * SPEED
+	move_and_slide(motion, Vector2(0, 0))
+	if(motion.x != 0 or motion.y != 0):
+		sprite.play()
+	else:
+		sprite.stop()
+
